4d7cf2c93f3ba80e2c72bff09a0fa7daf30fe4aa
Abhishree <abhishree881@gmail.com>
2023-09-13 23:43:30 +0530

    followbar

--- frontend/app/components/Followbar.tsx
+++ frontend/app/components/Followbar.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import '../../styles/followbar.css'
 
 type Props = {}
 
 const Followbar = (props: Props) => {
     return (
-        <div>Followbar</div>
+        <div className='followbar'>
+            <div className="follow-box">
+                <span className="follow-head">Who to follow</span>
+                <div className="follow-users">Userlist</div>
+            </div>
+        </div>
     )
 }
 
