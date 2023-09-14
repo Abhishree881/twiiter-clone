fdd97587abf751eca3152c227fff1bfbdec5a853
Abhishree <abhishree881@gmail.com>
2023-09-14 20:43:36 +0530

    modified

--- hooks/usePosts.ts
+++ hooks/usePosts.ts
@@ -3,7 +3,7 @@ import useSWR from 'swr';
 import fetcher from '@/libs/fetcher';
 
 const usePosts = (userId?: string) => {
-  const url = userId ? `https://twiiterx-corp.vercel.app/api/posts?userId=${userId}` : 'https://twiiterx-corp.vercel.app/api/posts';
+  const url = userId ? `/api/posts?userId=${userId}` : '/api/posts';
   const { data, error, isLoading, mutate } = useSWR(url, fetcher);
 
   return {
